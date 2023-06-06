a581e30b9719763e7c936436baa35a2b9449cfd8
Jean <jeansilvakorla@hotmail.com>
2023-06-06 13:36:31 -0300

    Exportação de Pdf to XML

--- app/Controllers/Base.php
+++ app/Controllers/Base.php
@@ -9,33 +9,32 @@ use CodeIgniter\HTTP\Files\UploadedFile;
 
 class Base extends BaseController
 {
-public function index()
-{
-    // Verifica se o usuário está autenticado
-    if (!session()->get('isLoggedIn')) {
-        
-         // Caso não esteja autenticado, redireciona para a tela de login
-        return redirect()->to('home/login');
-        
-    }
-    
+    public function index()
+    {
+        // Verifica se o usuário está autenticado
+        if (!session()->get('isLoggedIn')) {
+            // Caso não esteja autenticado, redireciona para a tela de login
+            return redirect()->to('home/login');
+        }
+
         echo view('common/header');
         echo view('index');
         echo view('common/footer');
- 
-}
-   public function fiscal()
+    }
+
+    public function fiscal()
     {
         echo view('common/header');
         echo view('pdf_upload');
         echo view('common/footer');
     }
-  public function convertPdfToText() {
+
+public function convertPdfToText() {
     // Verifica se o arquivo foi enviado corretamente
     if ($this->request->getMethod() === 'post') {
         $pdfFile = $this->request->getFile('pdf_file');
 
-        if ($pdfFile != null && $pdfFile->isValid() && ! $pdfFile->hasMoved()) {
+        if ($pdfFile != null && $pdfFile->isValid() && !$pdfFile->hasMoved()) {
             // Move o arquivo para um diretório temporário
             $newName = $pdfFile->getRandomName();
             $pdfFile->move('./uploads', $newName); // move o arquivo para a pasta /uploads
@@ -51,8 +50,18 @@ public function index()
                 // Extrai o texto do PDF
                 $text = $pdf->getText();
 
-                // Exibe o texto extraído
-                return view('pdf_result', ['text' => $text]);
+                //dd($text);
+                // Chama a função extractFields para obter os campos extraídos do texto
+                $fields = $this->extractFields($text);
+
+                // Formata os campos para exibição
+                $formattedFields = [];
+                foreach ($fields as $field) {
+                    $formattedFields[] = is_numeric($field) ? $field : '"' . $field . '"';
+                }
+
+                // Exibe o resultado na view ou passa para a próxima etapa do seu programa
+                return view('pdf_result', ['fields' => implode(',', $formattedFields)]);
             } catch (\Exception $e) {
                 // Lidar com erros, se houver
                 return view('pdf_error', ['error' => $e->getMessage()]);
@@ -64,5 +73,64 @@ public function index()
 
     return redirect()->back()->with('error', 'Nenhum arquivo PDF enviado.');
 }
-   
+
+function extractFields($text) {
+    $fields = [];
+
+    // Extrai o CNPJ
+    preg_match("/\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}/", $text, $matches);
+    $cnpj = isset($matches[0]) ? $matches[0] : '';
+
+    // Extrai o número da nota fiscal
+    preg_match("/SECRETARIA MUNICIPAL DE FINANÇAS\s*(\d+)/s", $text, $matches);
+    $numeroNota = isset($matches[1]) ? $matches[1] : '';
+
+    // Extrai data de emissão
+    preg_match("/SECRETARIA MUNICIPAL DE FINANÇAS\D*\d*.*(\d{2}\/\d{2}\/\d{4} \d{2}:\d{2})/s", $text, $matches);
+    $dataHoraEmissao = isset($matches[1]) ? \DateTime::createFromFormat('d/m/Y H:i', $matches[1])->format('Ymd') : '';
+
+    // O valor da nota fiscal
+    preg_match("/([\d\.,]+)\s*Avisos/", $text, $matches);
+    $valorNota = isset($matches[1]) ? (float)str_replace(',', '.', str_replace('.', '', $matches[1])) : 0.0;
+
+    // O valor de Base de Calculo
+    preg_match("/([\d\.,]+)\s*([\d\.,]+)\s*Avisos/s", $text, $matches);
+    $valorBase = isset($matches[1]) ? (float)str_replace(',', '.', str_replace('.', '', $matches[1])) : 0.0;
+
+    // Campo 1 - Número da chave da importação, neste caso estamos apenas usando um número aleatório
+    $fields[] = rand(1, 999999);
+
+    // Campo 2 - Código, CNPJ, CPF ou apelido do cliente
+    $fields[] = $cnpj;
+
+    // Campo 3 - Código da cidade. Neste exemplo, deixamos em branco
+    $fields[] = '5321';
+
+    // Campo 4 - Estado do destinatário da nota. Neste exemplo, deixamos em branco
+    $fields[] = 'SP';
+
+    // Campo 5 - Data da emissão da nota
+    $fields[] =  $dataHoraEmissao;
+
+    // Campo 6 - Número inicial das notas
+    $fields[] = $numeroNota;
+
+    // Campo 7 - Número final das notas
+    $fields[] = $numeroNota;
+
+    // Campo 8 - Espécie do documento, neste exemplo estamos usando um valor fixo
+    $fields[] = 'NFS';
+
+    // Campo 9 - Série do documento, neste exemplo estamos deixando em branco
+    $fields[] = '';
+
+    // Campo 10 - Valor contábil da nota
+    $fields[] = number_format($valorNota, 2, '.', '');
+
+    // Campo 11 - Valor contábil da nota
+    $fields[] = number_format($valorBase, 2, '.', '');
+
+    return $fields;
+}
+
 }
--- app/Views/pdf_upload.php
+++ app/Views/pdf_upload.php
@@ -1,3 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-dark mb-4" style="background-color: #1F628E;">
+  <div class="container-fluid navbar-container">
+    <a class="navbar-brand" href="#">
+        <img class="img-fluid" src="<?= base_url('img/logo-lado.svg') ?>" alt="Logolado">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <!-- Itens de navegação -->
+      </ul>
+      <!-- Formulário de pesquisa -->
+    </div>
+  </div>
+</nav>
 <!-- pdf_upload.php -->
 <!DOCTYPE html>
 <html>
