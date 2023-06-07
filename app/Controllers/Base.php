5f3529b4358a5fcb737b3c3aa0f28ffee5a748de
Jean <jeansilvakorla@hotmail.com>
2023-06-07 17:54:54 -0300

    Adicionando mais campos para parametrização

--- app/Controllers/Base.php
+++ app/Controllers/Base.php
@@ -49,7 +49,7 @@ public function convertPdfToText() {
                 // Extrai o texto do PDF
                 $text = $pdf->getText();
 
-                //dd($text);
+                dd($text);
                 // Chama a função extractFields para obter os campos extraídos do texto
                 $fields = $this->extractFields($text);
 
@@ -75,6 +75,8 @@ public function convertPdfToText() {
 
 function extractFields($text) {
     $fields = [];
+
+    $zero = 0.00;
 
     // Extrai o CNPJ
     preg_match("/\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}/", $text, $matches);
@@ -103,6 +105,11 @@ function extractFields($text) {
     //Valor ISS
     preg_match("/([\d\.,]+)\s*\(\X\) Sim \(\) Não/", $text, $matches);
     $valorIss = isset($matches[1]) ? (float)str_replace(',', '.', str_replace('.', '', $matches[1])) : 0.0;
+
+    //Valor IR
+    preg_match("/IR\(R\$\)\s*([\d\.,]+)/", $text, $matches);
+    $valorIR = isset($matches[1]) ? (float)str_replace(',', '.', str_replace('.', '', $matches[1])) : 0.0;
+
 
 
     // Campo 1 - Número da chave da importação, neste caso estamos apenas usando um número aleatório
@@ -146,6 +153,121 @@ function extractFields($text) {
 
     // Campo 14 - Observação, neste exemplo estamos deixando em branco
     $fields[] = '';
+
+    // Campo 15 - Valor contábil da nota (Valor base de cálculo do imposto de renda retido na fonte)
+    $fields[] = number_format($valorNota, 2, '.', '');
+
+    // Campo 16 - Alíquota do I.R.R.F 
+    $fields[] = number_format($zero, 2, '.', '');
+
+    // Campo 17 - Valor IR ----------------------------------------------------------------------------------
+    $fields[] = number_format($valorIR, 2, '.', '');
+
+    // Campo 18 - Condição pagamento da nota ----------------------------------------------------------------
+    $fields[] = '';
+
+    // Campo 19 - Conta Débito Exportação P/ contabilida
+    $fields[] = '';
+
+    // Campo 20 - Conta Crédito Exportação P/ contabilida
+    $fields[] = '';
+
+    // Campo 21 - Redução da base de cálculo de cálculo ----------------------------------------------------
+    $fields[] = '';
+
+    // Campo 22 - Valor de isentas do ISS
+    $fields[] = number_format($zero, 2, '.', '');
+
+    // Campo 23 - Valor de outras do ISS
+    $fields[] = number_format($zero, 2, '.', '');
+
+    // Campo 24 - Nome do primeiro centro de custo
+    $fields[] = '';
+
+    // Campo 25 -Percentual do primeiro Custo
+    $fields[] = number_format($zero, 2, '.', '');
+
+    // Campo 26 - Nome do segundo centro de custo
+    $fields[] = '';
+
+    // Campo 27 -Percentual do segundo Custo
+    $fields[] = number_format($zero, 2, '.', '');
+
+    // Campo 28 - Nome do terceiro centro de custo
+    $fields[] = '';
+
+    // Campo 29 -Percentual do terceiro Custo
+    $fields[] = number_format($zero, 2, '.', '');
+
+    // Campo 30 - Nome do quarto centro de custo
+    $fields[] = '';
+
+    // Campo 31 -Percentual do quarto Custo
+    $fields[] = number_format($zero, 2, '.', '');
+
+    // Campo 32 - Nome do quinto centro de custo
+    $fields[] = '';
+
+    // Campo 33 -Percentual do quinto Custo
+    $fields[] = number_format($zero, 2, '.', '');
+
+    // Campo 34 - Nome do sexto centro de custo
+    $fields[] = '';
+
+    // Campo 35 -Percentual do sexto Custo
+    $fields[] = number_format($zero, 2, '.', '');
+
+    // Campo 36 - Nome do setimo centro de custo
+    $fields[] = '';
+
+    // Campo 37 -Percentual do setimo Custo
+    $fields[] = number_format($zero, 2, '.', '');
+
+    // Campo 38 - Nome do oitavo centro de custo
+    $fields[] = '';
+
+    // Campo 39 -Percentual do oitavo Custo
+    $fields[] = number_format($zero, 2, '.', '');
+
+    // Campo 40 - Nome do nono centro de custo
+    $fields[] = '';
+
+    // Campo 41 -Percentual do nono Custo
+    $fields[] = number_format($zero, 2, '.', '');
+
+    // Campo 42 -Valor da retenção de INSS ----------------------------------------------------------------------
+    $fields[] = number_format($zero, 2, '.', '');
+
+    // Campo 43 - Tipo de Prestação de Serviço -----------------------------------------------------------------
+    $fields[] = '';
+
+    // Campo 44 -Tipo de recolhimento do ISSQN para cidades -----------------------------------------------------
+    $fields[] = '';
+
+    // Campo 45 -Código do Fornecedor, quando o estado da empresa for igual a MS e o tipo do Lançamento for R (Recebidos)
+    $fields[] = '';
+
+    // Campo 46 -Estado do Fornecedor, quando o estado da empresa for igual a MS e o tipo do Lançamento for R (Recebidos)
+    $fields[] = '';
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
     return $fields;
 }
