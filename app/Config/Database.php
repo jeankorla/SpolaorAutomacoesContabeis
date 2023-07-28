3c39e2fc96854d14cba1a701b4f56973e6e6a43f
JeanKorla <jeansilvakorla@hotmail.com>
2023-07-28 20:54:11 -0300

    modificações scia.com.br

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'      => '',
-        'hostname' => 'localhost',
-        'username' => '',
-        'password' => '',
-        'database' => '',
+        'hostname' => '50.116.86.134',
+        'username' => 'scsport_jean',
+        'password' => 'Jean1509!',
+        'database' => 'scsport_empenho',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
