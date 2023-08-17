fbe9283d927f8f447bbf2946a042e97afd8b0241
JeanKorla <jeansilvakorla@hotmail.com>
2023-08-17 12:03:05 -0300

    first commit

---
+++ .trash/app.1/Views/pdf_error.php
@@ -0,0 +1,12 @@
+<!-- pdf_error.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Erro</title>
+</head>
+<body>
+    <h1>Erro ao converter PDF para texto</h1>
+    
+    <div class="error"><?= $error ?></div>
+</body>
+</html>
---
+++ empenho/app/Views/manutencao.php
@@ -0,0 +1,38 @@
+<style> 
+body {
+  background-color: #eee;
+}
+img{
+    
+}
+</style>
+
+<nav class="navbar navbar-expand-lg navbar-dark mb-4" style="background-color:  #024A7F;">
+    <div class="container-fluid navbar-container">
+      <a class="navbar-brand" href="<?= base_url('base/index') ?>">
+          <img class="img-fluid" src="<?= base_url('img/logo-lado.svg') ?>" alt="Logolado">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <!-- Itens de navegação  teste-->
+        </ul>
+        <!-- Formulário de pesquisa -->
+      </div>
+    </div>
+  </nav>
+
+  <div class="container">
+    <div class="card">
+  <div class="card-body text-center">
+    Site atualmente em Manuteção.
+  </div>
+</div>
+<div class="" align="center">
+    <img src="<?= base_url('img/manutencao.svg') ?>" alt="">
+    </div>
+
+    <button href="<?= base_url('base/back') ?>">Voltar</button>
+  </div>
